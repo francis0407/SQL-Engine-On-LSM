55071d54ac7393d856138ec3c842277b13503e66
yc322 <bling_hong@163.com>
2019-05-18 12:50:39 +0800

    update

--- src/operators/CopyFile.h
+++ src/operators/CopyFile.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 
 #include "operators/Operator.h"
 #include "execution/LevelDB.h"
@@ -24,7 +25,42 @@ public:
     virtual ~CopyFile() {}
     
     virtual bool open() override {
-        
+        size_t num = ref.attributes.attributes.size();
+        std::ifstream fp(filePath.data);
+        char* str;  
+        while(fp.getline(str,1024)){
+            char* tmpStr = strtok(str, delimiter.data);
+            std::vector<string> res;
+            while(tmpStr != NULL) {
+                res.push_back(string(tmpStr));
+                tmpStr = strtok(NULL,delimiter.data);
+            }
+            MemoryPool* mp = new MemoryPool();
+            AnyValue** buf = (AnyValue**) mp->allocate(sizeof(AnyValue*) * ref.attributes.attributes.size());
+            AnyValue* values[num];
+            for(int i = 0 ;i< res.size();i++) {
+                DataType attribute = ref.attributes.attributes[i].dataType;
+                switch (attribute)
+                {
+                case Boolean :
+                    values[i] = BooleanValue::create(res[i]=="TRUE"?true:false,mp);
+                    break;
+                case String :
+                    values[i] = StringValue::create(res[i],mp);
+                    break;
+                case Integer:
+                    values[i] = IntegerValue::create(std::stoi(res[i]),mp);
+                    break;
+                case Float :
+                    values[i] = FloatValue::create(std::stof(res[i]),mp);
+                    break;
+                default:
+                    break;
+                }
+            }
+            Row* row = Row::create(values, num, mp);
+            LevelDB::putRow(SCHEMA_TABLE_ID, values[0], row);
+        }
         return true;
     }
 
