9a813073c093355a3c05f0592720afea207ceea9
francis0407 <hanmingcong123@hotmail.com>
2019-06-28 20:50:06 +0800

    Update homework

--- homework/main.cpp
+++ homework/main.cpp
@@ -23,7 +23,7 @@ using std::string;
 long getCurrentTime() {  
    struct timeval tv;  
    gettimeofday(&tv,NULL);  
-   return tv.tv_sec * 1000 + tv.tv_usec / 1000;  
+   return tv.tv_sec * 1000 * 1000 + tv.tv_usec;  
 }  
 
 void Prepare(string DBNAME, QueryExecutor& qe) {
@@ -70,7 +70,7 @@ void Query(string secondaryIndexKey, int lower, int upper, QueryExecutor& qe) {
     qe.executeTree(opt, result);
     long end_time = getCurrentTime();
     result.show();
-    std::cout << "Time: " << end_time - begin_time << " ms" << std::endl;
+    std::cout << "Time: " << end_time - begin_time << " us" << std::endl;
 }
 
 void test(string secondaryIndexKey, int lower, int upper, QueryExecutor& qe) {
@@ -113,14 +113,48 @@ void Query2(string secondaryIndexKey, int lower, int upper, QueryExecutor& qe) {
     qe.executeTree(opt, result);
     long end_time = getCurrentTime();
     result.show();
-    std::cout << "Time: " << end_time - begin_time << " ms" << std::endl;
+    std::cout << "Time: " << end_time - begin_time << " us" << std::endl;
+}
+
+void Query3(string secondaryIndexKey, int lower, int upper, QueryExecutor& qe) {
+    Relation result;
+    OperatorBase* opt = 
+        new InnerJoin(
+            new Filter(
+                new EqualTo(
+                    new AttributeReference("T1", "EMAIL"), Literal::create(secondaryIndexKey)
+                ),
+                new SeqScan(new RelationReference("EMAILS", "T1"), Literal::create(lower), Literal::create(upper))
+            ),
+            new Filter(
+                new And(
+                    new GreaterThanOrEqual(new AttributeReference("T2", "ID"), Literal::create(lower)),
+                    new LessThanOrEqual(new AttributeReference("T2", "ID"), Literal::create(upper))
+                ),
+                new SecondIndexScan(new RelationReference("EMAILS", "T2"), 1, Literal::create(secondaryIndexKey), Literal::create(secondaryIndexKey))
+            ),
+            BuildLeft,
+            new EqualTo(new AttributeReference("T1", "EMAIL"), new AttributeReference("T2", "EMAIL")),
+            HashJoin
+        );
+    ((InnerJoin*)opt)->leftKeyOffset = 1;
+    ((InnerJoin*)opt)->rightKeyOffset = 1;
+    long begin_time = getCurrentTime();
+    qe.executeTree(opt, result);
+    long end_time = getCurrentTime();
+    result.show();
+    std::cout << "Time: " << end_time - begin_time << " us" << std::endl;
 }
 
 int main(int argc, char** argv) {
     string DBNAME = string(argv[1]);
     QueryExecutor qe(new LevelDBCatalog(DBNAME));
-    Prepare(DBNAME, qe);
+    // Prepare(DBNAME, qe);
     // Query("WTZ@ecnu.cn", 1, 100000, qe);
+    // Query2("WTZ@ecnu.cn", 1, 100000, qe);
+    Query3("DOG@ecnu.cn", 1, 200000, qe);
+    
     // Query("UEL@ecnu.cn", 20000, 80000, qe);
-    Query2("NCC@ecnu.cn", 1, 200000, qe);
+    
+    // Query2("NCC@ecnu.cn", 1, 200000, qe);
 }
